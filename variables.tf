9cf072ae200182e6c5ac4c3e1704d5221cd5d250
veelash27 <64749834+veelash27@users.noreply.github.com>
2025-04-26 14:56:50 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,41 +1,34 @@
 variable "cidr_block" {
   description = "VPC CIDR"
-  type        = string
-  default     = "10.0.0.0/16" 
+  type        = string 
 }
 
 variable "vpc_name" {
   description = "VPC"
   type        = string
-  default     = "my-vpc"
 }
 
 variable "public_subnet_cidr" {
   description = "public subnet CIDR"
   type        = string
-  default     = "10.0.1.0/24"
 }
 
 variable "availability_zone" {
   description = "Availability zone for the public subnet"
   type        = string
-  default     = "us-east-1a"
 }
 
 variable "public_subnet_name" {
   description = "Name of the public subnet"
   type        = string
-  default     = "public-subnet"
 }
 
 variable "private_subnet_cidr" {
   description = "private subnet CIDR"
   type        = string
-  default     = "10.0.2.0/24"
 }
 
 variable "private_subnet_name" {
   description = "private subnet name"
   type        = string
-  default     = "private-subnet"
 }
