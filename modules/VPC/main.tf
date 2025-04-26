65c4d39dec3adb6ed9f4e57445a2265fb569e836
veelash27 <64749834+veelash27@users.noreply.github.com>
2025-04-26 18:02:36 +0530

    Create main.tf

---
+++ modules/VPC/main.tf
@@ -0,0 +1,29 @@
+resource "aws_vpc" "my_vpc" {
+  cidr_block           = var.cidr_block
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = var.vpc_name
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = var.public_subnet_cidr
+  map_public_ip_on_launch = true
+  availability_zone       = var.availability_zone
+
+  tags = {
+    Name = var.public_subnet_name
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = var.private_subnet_cidr
+
+  tags = {
+    Name = var.private_subnet_name
+  }
+}
